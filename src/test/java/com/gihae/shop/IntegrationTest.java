b9fedfea52aa2b795514460751470dbba1471ded
xcelxlorx <xcelxlorx@gmail.com>
2023-11-18 21:31:16 +0900

    feat: Add rest docs setup

--- src/test/java/com/gihae/shop/IntegrationTest.java
+++ src/test/java/com/gihae/shop/IntegrationTest.java
@@ -1,14 +1,28 @@
 package com.gihae.shop;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.nio.charset.StandardCharsets;
+
+@ExtendWith({ SpringExtension.class, RestDocumentationExtension.class })
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @Sql("classpath:db/teardown.sql")
@@ -21,4 +35,21 @@ public class IntegrationTest {
 
     @Autowired
     protected ObjectMapper om;
+
+    protected RestDocumentationResultHandler document;
+
+    @BeforeEach
+    private void setup(WebApplicationContext webApplicationContext,
+                       RestDocumentationContextProvider restDocumentation) {
+        this.document = MockMvcRestDocumentation.document("{class-name}/{method-name}",
+                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint()));
+
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true))
+                .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentation))
+                // .apply(SecurityMockMvcConfigurers.springSecurity())
+                .alwaysDo(document)
+                .build();
+    }
 }
